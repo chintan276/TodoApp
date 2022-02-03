4e9cc840638777d708a70b1c619624e5f5cec6b5
chintan276 <72489861+chintan276@users.noreply.github.com>
2022-02-03 22:41:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './App.css';
+import ReactDOM from "react-dom";
+import TodoForm from "./components/TodoForm";
+import './App.css';
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList />
+
+
+      
+    </div>
+  );
+}
+
+export default App;
